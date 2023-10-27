634a90e35dbbf34b9c396aeb7a1966947ac4b446
Viktor Major <majorviktor95@gmail.com>
2023-10-27 20:29:57 +0200

    add comments

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Game state variables
 let score = 0;
 let best = 0;
 const rows = 4;
@@ -5,13 +6,16 @@ const columns = 4;
 let winShown = false;
 let board;
 
+// Initialize the game when the window loads
 window.onload = function () {
   setGame();
 };
 
 const gameBoard = document.getElementById("board");
 
+// Set up the game board
 function setGame() {
+  // Initializing the board with zeros
   board = [
     [0, 0, 0, 0],
     [0, 0, 0, 0],
@@ -19,6 +23,7 @@ function setGame() {
     [0, 0, 0, 0],
   ];
 
+  // Populating the game board with tiles
   for (let r = 0; r < rows; r++) {
     for (let c = 0; c < columns; c++) {
       let tile = document.createElement("div");
@@ -32,6 +37,7 @@ function setGame() {
   setTwo();
 }
 
+// Check if the board has an empty tile
 function hasEmptyTile() {
   for (let r = 0; r < rows; r++) {
     for (let c = 0; c < columns; c++) {
@@ -43,6 +49,7 @@ function hasEmptyTile() {
   return false;
 }
 
+// Check if the board has possible moves
 function hasPossibleMoves() {
   for (let r = 0; r < rows; r++) {
     for (let c = 0; c < columns; c++) {
@@ -60,6 +67,7 @@ function hasPossibleMoves() {
   return false;
 }
 
+// Set a "2" on a random empty tile
 function setTwo() {
   if (!hasEmptyTile()) {
     return;
@@ -84,6 +92,7 @@ function setTwo() {
   }
 }
 
+// Update the appearance of a tile based on its number
 function updateTile(tile, num) {
   tile.innerText = "";
   tile.classList.value = "";
@@ -98,7 +107,10 @@ function updateTile(tile, num) {
   }
   win();
 }
+
 const scoreNum = document.getElementById("score-num");
+
+// Handle arrow key inputs to slide tiles
 document.addEventListener("keyup", (e) => {
   if (messageContainer.style.display === "none") {
     e.preventDefault();
@@ -123,10 +135,12 @@ document.addEventListener("keyup", (e) => {
   }
 });
 
+// Remove zeros from a row
 function filterZero(row) {
   return row.filter((num) => num !== 0);
 }
 
+// Slide and combine tiles in a row
 function slide(row) {
   row = filterZero(row);
 
@@ -146,6 +160,7 @@ function slide(row) {
   return row;
 }
 
+// Slide tiles to the desired direction
 function slideLeft() {
   for (let r = 0; r < rows; r++) {
     let row = board[r];
@@ -204,16 +219,23 @@ function slideDown() {
   }
 }
 
+// Initializes a new game
 function setNewGame() {
+  // Set an empty 4x4 board
   board = [
     [0, 0, 0, 0],
     [0, 0, 0, 0],
     [0, 0, 0, 0],
     [0, 0, 0, 0],
   ];
+  
+  // Check and set the best score
   bestScore();
+  
+  // Reset the current score
   score = 0;
 
+  // Update the display of each tile on the board
   for (let r = 0; r < rows; r++) {
     for (let c = 0; c < columns; c++) {
       let tile = document.getElementById(r.toString() + "-" + c.toString());
@@ -221,13 +243,22 @@ function setNewGame() {
       updateTile(tile, num);
     }
   }
+  
+  // Place two initial tiles with value '2' on the board
   setTwo();
   setTwo();
+
+  // Update the displayed score
   scoreNum.innerText = score;
+  
+  // Reset the win flag
   winShown = false;
+  
+  // Clear any previous win/lose message
   deleteMessage();
 }
 
+// Update and display the best score if current score exceeds it
 function bestScore() {
   if (score > best) {
     best = score;
@@ -235,9 +266,11 @@ function bestScore() {
   }
 }
 
+// Select the message container and initially hide it
 const messageContainer = document.getElementById("message");
 messageContainer.style.display = "none";
 
+// Function to display a win message when a player reaches 2048
 function win() {
   if (board.flat().includes(2048) && !winShown) {
     messageContainer.style.display = "block";
@@ -250,23 +283,27 @@ function win() {
   }
 }
 
+// Function to clear the win/lose message
 function deleteMessage() {
   messageContainer.style.display = "none";
   messageContainer.innerText = "";
 }
 
+// Function to display a lose message when there are no more valid moves
 function lose() {
   messageContainer.style.display = "flex";
   messageContainer.innerHTML = `<p>Game over!</p>
     <button onclick ="setNewGame()">Try again!</button>`;
 }
 
+// Event listener to handle theme toggling (between dark and light modes)
 document.addEventListener("DOMContentLoaded", function () {
   const modeToggle = document.getElementById("mode-toggle");
 
   modeToggle.addEventListener("click", function () {
     const body = document.body;
 
+    // Switch between light and dark themes
     if (body.classList.contains("light-mode")) {
       body.classList.remove("light-mode");
       body.classList.add("dark-mode");
